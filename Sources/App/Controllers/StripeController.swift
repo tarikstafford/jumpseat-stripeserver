43b52a4c9f304848667edc3ff34f15c16dd8695a
tarik <tarikstafford@gmail.com>
2018-04-19 14:55:27 +0530

    Creating Remote

--- Sources/App/Controllers/StripeController.swift
+++ Sources/App/Controllers/StripeController.swift
@@ -34,31 +34,7 @@ final class StripeController {
         let response = try stripeClient.ephemeralKey.create(customer: customerId)
         
         return response
-        
-//        return try req.content.decode(StripeCustomer.self).flatMap(to: StripeEphemeralKey.self, { (customer) in
-//            guard let customerId = customer.id else { throw Abort(.badRequest, reason: "No Customer Id")}
-//            let response = try stripeClient.ephemeralKey.create(customer: customerId)
-//            return response
-//        })
     }
-
-    
-    // Not required if you use the standard integration elements.
-//    func createCard(_ req: Request) throws -> Future<StripeToken> {
-//
-//        let stripeClient = try req.make(StripeClient.self)
-//
-//        let customer = try req.content.syncDecode(StripeCustomer.self)
-//
-//        var credit = try req.content.syncDecode(StripeCard.self)
-//        credit.id = nil
-//
-//        guard let customerId = customer.id else { throw Abort(.badRequest, reason:"No Customer Id")}
-//
-//        let response = try stripeClient.token.createCard(card: credit.toEncodedDictionary(), customer: customerId)
-//
-//        return response
-//    }
     
     func getCustomer(_ req: Request) throws -> Future<StripeCustomer> {
         
