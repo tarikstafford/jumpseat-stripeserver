4a417cea0e382456a4f2cba1954f67699f40990f
tarik <tarikstafford@gmail.com>
2018-05-10 17:28:35 +0530

    updated again

--- Package.swift
+++ Package.swift
@@ -7,14 +7,11 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        
         // Stripe Package
         .package(url: "https://github.com/vapor-community/stripe-provider.git", from: "2.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Stripe"]),
+        .target(name: "App", dependencies: ["Vapor", "Stripe"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
